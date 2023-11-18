6fea7df5cd64c3cc573aca1b095de477bf524d2a
ofer026 <ofer026@gmail.com>
2023-11-18 22:57:37 +0200

    Create a room class to group the users that are connected to the file

---
+++ server/room.py
@@ -0,0 +1,16 @@
+from file_system_wrapper import FSWrapper
+from user import User
+
+class Room():
+    def __init__(self, file_path, file_name):
+        self._file_name = file_name
+        self._file_path = file_path
+        self._file_object = FSWrapper.open_file(file_path, file_name, "r+")
+        self._users = []
+    
+    def add_user(self, user):
+        self._users.append(user)
+    
+    def get_file_name(self):
+        return self._file_name
+    
