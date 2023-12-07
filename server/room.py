dabbf33a28b6f8bb46a8ee11a48b175713d00c64
Shaked Sisso <sissoshaked@gmail.com>
2023-12-07 19:04:53 +0200

    Added a function that sends changes to all users in the room accept the user that sent the changes

--- server/room.py
+++ server/room.py
@@ -1,5 +1,6 @@
 from file_system_wrapper import FSWrapper
 import threading
+import json
 
 class Room():
     def __init__(self, file_path, file_name):
@@ -19,3 +20,11 @@ class Room():
         with self._file_lock:
             for line_number, new_line in changes.items():
                 FSWrapper.write_change_to_file(self._file_object, line_number, new_line)
+        
+    def send_changes_to_all_room_users(self, data, sending_user):
+        data["code"] = 2 #add the update code to the data dictionary
+        print(data)
+        for user in self._users:
+            if user.get_user_socket() != sending_user.get_user_socket():
+                data_json = json.dumps(data) 
+                user.send_message(data_json.encode())
