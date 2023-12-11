f87c0f3eb55892fed434d07da7531e025d079eb7
Shaked Sisso <sissoshaked@gmail.com>
2023-12-11 17:12:21 +0200

    Created the basic functions needed in the database wrapper for saving and extracting data from the mongoDB database

---
+++ server/database_wrapper.py
@@ -0,0 +1,34 @@
+from pymongo import MongoClient
+
+class MongoDBWrapper:
+    # Set your MongoDB connection details here
+    DATABASE_NAME = 'codeHarbor'
+
+    @staticmethod
+    def connect_to_mongo(collection_name):
+        client = MongoClient('mongodb://localhost:27017/')
+        return client[MongoDBWrapper.DATABASE_NAME][collection_name] #returns the collection with the given name in the database "codeHarbor"
+
+    @staticmethod
+    def insert_document(data, collection):
+        result = collection.insert_one(data)
+        return result.inserted_id
+
+    @staticmethod
+    def find_document(query, collection):
+        return collection.find_one(query)
+
+    @staticmethod
+    def update_document(query, update_data, collection):
+        result = collection.update_one(query, {'$set': update_data})
+        return result.modified_count
+
+    @staticmethod
+    def delete_document(query, collection):
+        result = collection.delete_one(query)
+        return result.deleted_count
+
+    @staticmethod
+    def find_documents(query, collection):
+        documents = collection.find(query)
+        return list(documents)
