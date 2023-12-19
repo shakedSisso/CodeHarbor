0ff03874667d94117962c457ef7df4aa324fe624
Ofer Ovadia <ofer026@gmail.com>
2023-12-20 00:45:53 +0200

    Added an function to trigger the menu creation in preload.js

--- Client/preload.js
+++ Client/preload.js
@@ -62,6 +62,12 @@ socket.on('data', (data) => {
   }
 });
 
+contextBridge.exposeInMainWorld('api', {
+  setMenu: (menu) => {
+    ipcRenderer.send('set-menu', menu);
+  },
+});
+
 contextBridge.exposeInMainWorld('electronAPI', {
   sendChanges: (changes) => ipcRenderer.invoke('dialog:sendChanges', changes),
   getContentFile: (callback) => ipcRenderer.on('file-content', callback),
