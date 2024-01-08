043673edeff1290b15593f02973a9dbb3eb770be
Shaked Sisso <sissoshaked@gmail.com>
2024-01-08 21:16:43 +0200

    Created a preload files for a context bridge which is used for connecting functions in the renderer and the window files

---
+++ Client/login/loginPreload.js
@@ -0,0 +1,6 @@
+const { ipcRenderer, contextBridge } = require('electron');
+
+contextBridge.exposeInMainWorld('electronAPI', {
+  switchToSignup: () => ipcRenderer.invoke('dialog:switchToSignup'),
+  sendLoginDetails: (username, password) => ipcRenderer.invoke('dialog:sendLoginDetails', username, password),
+})
---
+++ Client/signup/signupPreload.js
@@ -0,0 +1,6 @@
+const { ipcRenderer, remote, contextBridge } = require('electron');
+
+contextBridge.exposeInMainWorld('electronAPI', {
+  switchToLogin: () => ipcRenderer.invoke('dialog:switchToLogin'),
+  sendSignUpDetails: (username, password, email) => ipcRenderer.invoke('dialog:sendSignUpDetails', username, password, email),
+})
